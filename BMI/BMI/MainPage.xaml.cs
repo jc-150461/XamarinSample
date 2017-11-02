205e8a317a42e5bf7afadcbbbfd46015e09d1dca
燎 石橋 <150461@jc-21.jp>
2017-11-02 16:19:01 +0900

    初回

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -13,5 +13,25 @@ namespace BMI
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            Double h = Double.Parse(Height.Text);
+            Double w = Double.Parse(Weight.Text);
+
+            Double bmi = 0.0;
+
+            DisplayAlert("身長", h.ToString(), "OK");
+            DisplayAlert("体重", w.ToString(), "OK");
+            if( h >= 3)
+            {
+                h /= 100;
+            }
+             bmi = w / (h * h);
+            
+            DisplayAlert("BMI",bmi.ToString() , "OK");
+           
+        }
+    }
 }
