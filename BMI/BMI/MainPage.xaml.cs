7eb76f3277f68f40d8dad9e14772b0a38f4ff727
燎 石橋 <150461@jc-21.jp>
2017-11-07 16:30:57 +0900

    初回

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -7,31 +7,52 @@ using Xamarin.Forms;
 
 namespace BMI
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            
+
             Double h = Double.Parse(Height.Text);
             Double w = Double.Parse(Weight.Text);
 
             Double bmi = 0.0;
 
-            DisplayAlert("身長", h.ToString(), "OK");
-            DisplayAlert("体重", w.ToString(), "OK");
-            if( h >= 3)
+            //DisplayAlert("身長", h.ToString(), "OK");
+            //DisplayAlert("体重", w.ToString(), "OK");
+            if (h >= 3)
             {
                 h /= 100;
             }
              bmi = w / (h * h);
+
+            DisplayAlert("BMI", bmi.ToString(), "OK");
+            if ( bmi < 16)
+            {
+                DisplayAlert("BMI", "痩せすぎ", "OK");
+            }
+            else if (bmi <= 16.99)
+            {
+                DisplayAlert("BMI", "痩せ", "OK");
+            }
+            else if (bmi <= 18.49)
+            {
+                DisplayAlert("BMI", "痩せぎみ", "OK");
+            }
+            else if (bmi >= 25.0)
+            {
+                DisplayAlert("BMI", "太りすぎ", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", "普通", "OK");
+            }
             
-            DisplayAlert("BMI",bmi.ToString() , "OK");
-           
         }
     }
 }
+
